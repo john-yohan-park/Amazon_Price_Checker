26497d3c0bc2b4ef8aae43be94f171aaf17302c9
johnSMUpark <park@smu.edu>
2019-12-21 02:19:51 -0800

    w

--- amazon.py
+++ amazon.py
@@ -4,43 +4,45 @@ Github  john-yohan-park
 Date    10/16/2019
 
 System Requirements
-    Name:           Mac Terminal Command to Install:
+    Name:           Command:
     Homebrew        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     Python 3        brew install python
-    Requests        pip3 install requests
-    Beautiful Soup  pip3 install beautifulsoup4
-    Gecko Driver    brew install geckodriver
+    Requests        pip3 install requests           to send HTTP requests
+    Beautiful Soup  pip3 install beautifulsoup4     parsing html files
+    Gecko Driver    brew install geckodriver        link between Selenium / Beautiful Soup and Firefox
     Firefox         brew cask install firefox
 
 Program Requirements
-    Sender's email
-        - Needs to be gmail with 2-step verification (visit google.com/landing/2step)
+    Sender's email needs to be gmail with 2-step verification (google.com/landing/2step)
     Recipient's email
     Google App Password
-        - visit accounts.google.com and log-in with sender's email
+        - accounts.google.com
+        - login with sender's email
     Your User Agent
-        - Google 'my user agent' and copy & paste top result
-    URL of the desired item from Amazon.com
+        - google 'my user agent' and copy & paste result
+    URL of desired item from Amazon.com
     Max price you're willing to pay
 
-To run this program, type into the terminal: python3 amazon.py
+To run, type into terminal: python3 amazon.py
 To exit, press CTRL + C
 """
 
-import requests
-from bs4 import BeautifulSoup
-import smtplib
-import time
+# import libraries
+import  requests
+import  smtplib
+import  time
+from    bs4 import BeautifulSoup
 
-sender_email = 'abc123@gmail.com'
-recipient_email = 'def456@gmail.com'
+# define variables
+sender_email        = 'abc123@gmail.com'
+recipient_email     = 'def456@gmail.com'
 google_app_password = 'xyz789'
-URL = 'www... '
-userAgent = {'User-Agent': 'abcd'}
-max_price = 123     # enter XXX or XXX.YY. No commas and dollar signs.
+URL                 = 'www... '
+userAgent           = {'User-Agent': 'abcd'}
+max_price           = 123 # enter XXX or XXX.YY. No commas and dollar signs
 
 def check_price():
-    page = requests.get(URL, headers = userAgent)
+    page  = requests.get(URL, headers = userAgent)
     soup1 = BeautifulSoup(page.content, 'html.parser')
     soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
 
@@ -50,8 +52,7 @@ def check_price():
     float_price = float(price)                                  # convert str to float
     str_price = '{:.2f}'.format(round(float_price, 2))          # round to nearest cent
 
-    if(float_price <= max_price):
-        send_email()
+    if(float_price <= max_price): send_email()
 
 def send_email():
     server = smtplib.SMTP('smtp.gmail.com', 587)    # TLS encryption port
@@ -60,7 +61,7 @@ def send_email():
     server.ehlo()       # re-establish connection
     server.login(sender_email, google_app_password)
 
-    subject = "This item meets your desired price point of $" + str(max_price)
+    subject = "This item meets your desired price point $" + str(max_price)
     body = 'Visit ' + URL
     msg = f'Subject: {subject}\n\n{body}'
     server.sendmail(sender_email, recipient_email, msg)
