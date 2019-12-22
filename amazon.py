935f9992a73b472d9a9bb23c0f288bbd5c7139f4
johnSMUpark <park@smu.edu>
2019-12-22 00:40:55 -0800

    w

--- amazon.py
+++ amazon.py
@@ -33,28 +33,36 @@ import smtplib
 import time
 from   bs4 import BeautifulSoup
 
-# define variables
+# initialize local variables whose scope exist only within this program file 'amazon.py'
 sender_email        = 'annie.xiu.lam@gmail.com' # email password: beyonce365
-google_app_password = 'tykgzchypgcbrurh'        # not email password
+google_app_password = 'tykgzchypgcbrurh'        # not email password. It should be a 16-character password google generates for you
 recipient_email     = 'john.vegan.park@gmail.com'
 URL                 = 'https://www.amazon.com/AbergBest-Rechargeable-Digital-Students-cameras/dp/B078YR3MNK/ref=sr_1_1?keywords=camera&qid=1576992299&sr=8-1'
 userAgent           = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
 max_price           = 45  # enter XXX or XXX.YY. No commas and dollar signs
 
+# check_price function
 def check_price():
-    page  = requests.get(URL, headers = userAgent)
-    soup1 = BeautifulSoup(page.content, 'html.parser')
-    soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
-
-    global title 
-    title = soup2.find(id = 'productTitle').get_text().strip()
-    price = soup2.find(id = 'priceblock_ourprice').get_text()   # str $1,998.00
+    page  = requests.get(URL, headers = userAgent)         # using the URL, get page
+    soup1 = BeautifulSoup(page.content, 'html.parser')     # scrape the page's HTML contents
+    soup2 = BeautifulSoup(soup1.prettify(), 'html.parser') # do it again, so we can format its contents
+                                                           # we have to parse its HTML contents 2X because
+                                                           # Amazon.com generates its HTML pages automatically
+    #print(soup2)       # uncomment this line if you want to see extracted HTML contents
+    global title
+    title = soup2.find(id = 'productTitle').get_text().strip()  # scrape product title from parsed HTML contents
+    price = soup2.find(id = 'priceblock_ourprice').get_text()   # scrape product price
     price = price.replace('$', '').replace(',', '')             # remove $ and ,
     float_price = float(price)                                  # convert str to float
     #str_price = '{:.2f}'.format(round(float_price, 2))         # round to nearest cent
+    global emailHasBeenSent
+    emailHasBeenSent = False
 
-    if(float_price <= max_price): send_email()
+    if(float_price <= max_price): 
+        send_email()
+        emailHasBeenSent = True
 
+# send_email function
 def send_email():
     server = smtplib.SMTP('smtp.gmail.com', 587)    # TLS encryption port
     server.ehlo()       # establish connection with email server
@@ -69,6 +77,8 @@ def send_email():
     print('Email has been sent')
     server.quit()
 
-while(1):              # infinite loop
+# main function
+while(True):           # infinite loop
     check_price()
+    if(emailHasBeenSent): break
     time.sleep(3600)   # check every hour
